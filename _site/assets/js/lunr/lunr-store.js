f3ea087755bb641d126302c446a238fccbab71b7
bohanw <bohanwang93@gmail.com>
2024-06-08 23:29:03 -0700

    Revert "style"
    This reverts commit 63f5157dbf0240ba9411fb851a566ff2bec1ecc8.

--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -4,10 +4,4 @@ var store = [{
         "tags": [],
         "url": "/jekyll/update/2024/05/16/welcome-to-jekyll.html",
         "teaser": null
-      },{
-        "title": "Overview of KV store interview problems",
-        "excerpt":"Over the several months of interviews I frequently encountered problems to design KV store, cache, of various flavors. This post is intended to summarize thought processes to following problems, and to identify any patterns, if possible, to similar problems. For simplicity, I will assume all key values in these problems...","categories": [],
-        "tags": [],
-        "url": "/2024/05/17/overview-of-cache.html",
-        "teaser": null
       }]
